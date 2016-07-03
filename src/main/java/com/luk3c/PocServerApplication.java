409fab4c84cb016645b8dade7486e24e14cf5328
LukasSmrcka <lukas.smrcka@gmail.com>
2016-07-03 22:03:05 +0200

    Initial commit

---
+++ src/main/java/com/luk3c/PocServerApplication.java
@@ -0,0 +1,14 @@
+package com.luk3c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PocServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocServerApplication.class, args);
+	}
+}
